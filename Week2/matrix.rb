46712216f0a46afd4a5b529137c7d95e2b2de628
17Takafumi <dieu.detruit@gmail.com>
2017-10-10 18:25:53 +0900

    finish the homework matrix.rb

--- Week2/matrix.rb
+++ Week2/matrix.rb
@@ -7,7 +7,25 @@ def multMat(x, y)
 	if n != y.length then
 		print "Multi Matrix is not defined"
 	end 
+
+	matrix = Array.new(m){Array.new(k){0}}
+	
+	m.times do |i|
+		k.times do |j|
+			n.times do |s|
+				matrix[i][j] += x[i][s] * y[s][j]
+			end
+		end
+	end
+
+
+	matrix
 end
 
 
-print multMat([[1, 0],[0, 1]], [[2, 3], [4, 1]])
+a = multMat([[1, 5],[3, 1], [2, 2]], [[2, 3, 1], [4, 1, 2]])
+
+
+print(a[0][0], ", ", a[0][1], ", ", a[0][2], "\n")
+print(a[1][0], ", ", a[1][1], ", ", a[1][2], "\n")
+print(a[2][0], ", ", a[2][1], ", ", a[2][2], "\n")
