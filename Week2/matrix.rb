cdd3a8910c9455f8821d07b0cf145d1c9330e583
17Takafumi <dieu.detruit@gmail.com>
2017-10-10 17:24:39 +0900

    define matrix function

--- Week2/matrix.rb
+++ Week2/matrix.rb
@@ -1,4 +1,13 @@
-def mult
+def multMat(x, y)
 
+	m = x.length
+	n = x[0].length
+	k = y[0].length
 
+	if n != y.length then
+		print "Multi Matrix is not defined"
+	end 
 end
+
+
+print multMat([[1, 0],[0, 1]], [[2, 3], [4, 1]])
