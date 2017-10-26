98248bd90df9c8a09c171570f44f07b2089071de
17Takafumi <dieu.detruit@gmail.com>
2017-10-26 17:00:10 +0900

    画像貼り付け用キャンパスクラスを作った

--- Week3/freeimage.rb
+++ Week3/freeimage.rb
@@ -1,24 +1,30 @@
 #-*- coding: utf-8 -*-
 include(Math)
 
-def make2d(height, width)
-  Array.new(height){ Array.new(width,0) }
+class Campus
+
+public
+	def initialize(width, height)
+		@plane = Array.new(height){Array.new(width, [1, 1, 1])}
+		@width = width
+		@height = height
+	end
+	
+	#上書き描画
+	def appendDraw(image, x, y)
+		image.length().times do |i|
+			break if y+i>height
+			image[0].length().times do |j|
+				break if x+j>width
+				@plane[y+i][x+j] = image[i][j]
+			end
+		end
+	end
+
+	def getPlain()
+		return @plane;
+	end
+
 end
 
-def brightness(s, x, y)
-  if y==0
-    [sin(x), sin(y), 0]
-  else
-    [sin(x), sin(y), x/y]
-  end
-end
 
-def myimage(s)
-  image = make2d(s,s)
-  for y in 0..(s-1)
-    for x in 0..(s-1)
-      image[y][x] = brightness(s,x,y)
-    end
-  end
-  image
-end
