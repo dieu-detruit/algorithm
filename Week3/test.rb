0c1c03fdc361ca0cf0689f65dd9e4680d7998f32
17Takafumi <dieu.detruit@gmail.com>
2017-11-06 02:32:48 +0900

    はい、できた

--- Week3/test.rb
+++ Week3/test.rb
@@ -1,7 +1,17 @@
-require 'chunky_png'
+include(Math)
 
-Height = 400
-Width = 400
+require 'chunky_png' 
+Height = 480
+Width = 300
+
+def flameseed(x)
+	return 0.7*sin(4.0*x) + 0.3
+end
+
+def flamerise(x)
+	x = 1.4 * (x - 3.0)
+	return (cos(4.0*x) + cos(10.0*x) + sin(5.0*x))/3.0
+end
 
 # GhunkyPNGの描画用キャンバス
 png = ChunkyPNG::Image.new(Width, Height, ChunkyPNG::Color::TRANSPARENT)
@@ -28,21 +38,31 @@ end
 
 
 # 炎の作成
-100.times do
+18.times do |count|
 	# 火種をまく
 	(Height+1).downto(Height) do |i|
 		Width.times do |j|
-			image[i][j] = (rand(2) == 0) ? 0 : 127
+			image[i][j] = rand < 0.6 ? 0 : 127
 		end
 	end
 
-	r = 2*rand
+=begin
+	(Height+1).downto(Height) do |i|
+		Width.times do |j|
+			var = j/Width.to_f
+			temp = flameseed(var)
+			image[i][j] = (temp > -0.3) ? 127.0 * temp.abs : 0
+		end
+	end
+=end
 
-	# ぼかしながらし押上げ
 	(Height-1).downto(0) do |i|
 		(Width-2).downto(1) do |j|
-			temp = (image[i+1][j] + r*image[i+1][j-1] + (2-r)*image[i+1][j] + image[i+1][j+1] + image[i+2][j])/5.0;
-			image[i][j] = ((temp<50) ? temp + 5.0*rand : temp).to_i
+			r1 = 0.6 + 0.4 * cos(j)
+			r2 = 0.4 - 0.4 * cos(j)
+			
+			temp = (0.2*image[i][j] + (0.5+r1)*image[i+1][j-1] + (1.0-r1+r2)*image[i+1][j] + (1.5-r2)*image[i+1][j+1])/3.2
+			image[i][j] = temp + ((temp<64) ? temp : 127-temp) * (flamerise((j.to_f/Width - 0.5)))**2 * rand * 0.013
 		end
 	end
 end
@@ -51,7 +71,7 @@ end
 # ChunkyPNGキャンバスに転写
 (Height).times do |i|
 	(Width).times do |j|
-		png[j, i] = ChunkyPNG::Color.rgb(color[image[i][j]][0], color[image[i][j]][1], color[image[i][j]][2])
+		png[j, i] = ChunkyPNG::Color.rgb(color[image[i][j].to_i][0], color[image[i][j].to_i][1], color[image[i][j].to_i][2])
 	end
 end
 
