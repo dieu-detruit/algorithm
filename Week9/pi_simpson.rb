6193f57f3e77374f6d1cd4a68024259bc183bf14
17Takafumi <dieu.detruit@gmail.com>
2017-12-15 16:34:56 +0900

    compare the accuracy of trapeziod and simpson

--- Week9/pi_simpson.rb
+++ Week9/pi_simpson.rb
@@ -8,19 +8,27 @@ end
 
 def area_circle(n)
 	dx = 0.5 / n.to_f
-	return (1..(n-1)).inject {|sum, i|
+	return dx / 3.0 * (0..(n-1)).inject(0){|sum, i|
 		sum + f(2.0*i.to_f*dx) + 4.0*f((2.0*i.to_f+1.0)*dx) + f((2.0*i.to_f+2.0)*dx)
-	} * dx / 3.0
+	}
 end
 
 def pi(n)
 	return 4.0 * area_circle(n)
 end
 
-test = [5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000, 200000, 500000, 1000000]
+
+1.upto(30) do |i|
+	print(i, ", ", pi(i), "\n")
+end
+
+6.upto(40) do |i|
+	print(i*5, ", ", pi(i*5), "\n")
+end
+=begin
+test = [5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000, 200000, 500000, 1000000, 2000000, 5000000]
 
 test.each {|i|
 	print(i, ", ", pi(i), "\n")
 }
-
-
+=end
--- Week9/pi_trapezoid.rb
+++ Week9/pi_trapezoid.rb
@@ -10,19 +10,19 @@ end
 
 def area_circle(n)
 	dx = 1.0 / n.to_f
-	return (1..(n-1)).inject {|sum, i|
+	return dx / 2.0 * (0..(n-1)).inject(0){|sum, i|
 		sum + f(i.to_f * dx) + f((i+1).to_f * dx)
-	} * dx * 0.5
+	}
 end
 
 def pi(n)
 	return 4.0 * area_circle(n)
 end
 
-test = [5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000, 200000, 500000, 1000000]
-
-test.each {|i|
+1.upto(30) do |i|
 	print(i, ", ", pi(i), "\n")
-}
+end
 
-
+6.upto(40) do |i|
+	print(i*5, ", ", pi(i*5), "\n")
+end
