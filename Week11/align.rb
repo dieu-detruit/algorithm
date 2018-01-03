df282b3d5bd9a63e91c0ffef85f3a21f7f6086d8
17Takafumi <dieu.detruit@gmail.com>
2018-01-03 17:19:19 +0900

    add align_dp.rb

---
+++ Week11/align.rb
@@ -0,0 +1,41 @@
+def g()
+	return -2
+end
+
+def q(c, d)
+	return (c == d) ? 2 : -1
+end
+
+def max(*args)
+	return args.inject(args[0]) { |max, item|
+		max < item ? item : max
+	}
+end
+
+def align(s,t)
+	m = s.length
+	n = t.length
+	a = Array.new(m+1){Array.new(n+1)}
+	a[0][0] = 0
+
+	for j in 1..n
+		a[0][j] = a[0][j-1] + g
+	end
+
+	for i in 1..m
+		a[i][0] = a[i-1][0] + g
+	end
+
+	for i in 1..m
+		for j in 1..n
+			a[i][j] = max(a[i][j-1] + g, a[i-1][j-1] + q(s[i-1], t[j-1]), a[i-1][j] + g)
+		end
+	end
+	return a
+end
+
+def align_dp(s, t)
+	align(s, t)[s.length][t.length]
+end
+
+p align_dp("AAC", "ATAG")
--- Week11/align_rec.rb
+++ Week11/align_rec.rb
@@ -14,7 +14,7 @@ end
 
 def align_sub(s,t,i,j)
 	if i==0 || j==0
-		i*g() + j*g()
+		i*g + j*g
 	else
 		v0 = align_sub(s,t,i, j-1) + g()
 		v1 = align_sub(s,t,i-1,j-1) + q(s[i-1],t[j-1])
